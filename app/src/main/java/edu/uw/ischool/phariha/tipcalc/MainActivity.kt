9cdde2b8b4eef4da2cc9bd533b612cc7e0715cbd
phariha <phariha@uw.edu>
2023-10-26 15:56:45 -0700

    minor edits, added dollar sign and other user story edits

--- app/src/main/java/edu/uw/ischool/phariha/tipcalc/MainActivity.kt
+++ app/src/main/java/edu/uw/ischool/phariha/tipcalc/MainActivity.kt
@@ -2,20 +2,44 @@ package edu.uw.ischool.phariha.tipcalc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.Button
+import android.widget.EditText
+import com.google.android.material.textfield.TextInputEditText
 import edu.uw.ischool.phariha.tipcalc.databinding.ActivityMainBinding
 import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-
+    private lateinit var costOfServiceEditText: EditText
+    private lateinit var tipButton: Button
+    private lateinit var serviceButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         binding.calculateButton.setOnClickListener { calculateTip() }
+        costOfServiceEditText = findViewById(R.id.cost_of_service)
+        tipButton = findViewById(R.id.calculate_button)
+        serviceButton = findViewById(R.id.service_question)
+        tipButton.isEnabled = false
+        serviceButton.isEnabled = false
+
+        costOfServiceEditText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+            override fun afterTextChanged(s: Editable?) {
+//                costOfServiceEditText.setText("$" + costOfServiceEditText.text.toString());
+                tipButton.isEnabled = !s.isNullOrBlank()
+                serviceButton.isEnabled = !s.isNullOrBlank()
+
+            }
+        })
+
     }
 
     private fun calculateTip() {
