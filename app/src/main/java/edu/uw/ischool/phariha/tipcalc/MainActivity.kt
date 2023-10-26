66cc542a755dadc3bb41b6d87d96e905ef167f02
phariha <phariha@uw.edu>
2023-10-26 15:57:19 -0700

    better spacing

--- app/src/main/java/edu/uw/ischool/phariha/tipcalc/MainActivity.kt
+++ app/src/main/java/edu/uw/ischool/phariha/tipcalc/MainActivity.kt
@@ -33,10 +33,8 @@ class MainActivity : AppCompatActivity() {
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
 
             override fun afterTextChanged(s: Editable?) {
-//                costOfServiceEditText.setText("$" + costOfServiceEditText.text.toString());
                 tipButton.isEnabled = !s.isNullOrBlank()
                 serviceButton.isEnabled = !s.isNullOrBlank()
-
             }
         })
 
